7e7f8b2e8fa2b95f2d715177f95b2b0e07c02435
jeslin <jeslinjesi731@gmail.com>
2025-04-11 10:46:29 +0530

    added

---
+++ src/app/leaveform/leaveform.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leaveform',
+  imports: [],
+  templateUrl: './leaveform.component.html',
+  styleUrl: './leaveform.component.css'
+})
+export class LeaveformComponent {
+
+}
